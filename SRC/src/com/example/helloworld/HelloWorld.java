564fd2ac75772c7d913c0c153d279f71e68f41c9
therealstan <stanislav.sokol@gmail.com>
2014-10-14 11:30:22 +0200

    HelloWorld in IntelliJ

---
+++ SRC/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+/**
+ * Created by Stanislav on 14.10.2014.
+ * HelloWorld!
+ */
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
