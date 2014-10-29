a12f8d55fdfd6d3649409ca54942031f1e38cc0a
therealstan <stanislav.sokol@gmail.com>
2014-10-29 16:06:41 +0100

    Project initial commit

---
+++ SE_PROJECT/src/User.java
@@ -0,0 +1,170 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+@ManagedBean(name = "user")
+@SessionScoped
+public class User {
+
+    private String name;
+    private String password;
+    private String dbPassword;
+    private String dbName;
+    DataSource ds;
+
+    boolean isLoginPage = (FacesContext.getCurrentInstance().getViewRoot()
+            .getViewId().lastIndexOf("login.xhtml") > -1);
+
+    public User() {
+        try {
+
+
+            Context ctx = new InitialContext();
+            ds = (DataSource)ctx.lookup("java:comp/env/jdbc/database");
+        } catch (NamingException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    public String getDbPassword() {
+        return dbPassword;
+    }
+
+    public String getDbName() {
+        return dbName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String add() {
+        int i = 0;
+        if (name != null) {
+            PreparedStatement ps = null;
+            Connection con = null;
+            try {
+                if (ds != null) {
+                    con = ds.getConnection();
+                    if (con != null) {
+                        String sql = "INSERT INTO user(name, password) VALUES(?,?)";
+                        ps = con.prepareStatement(sql);
+                        ps.setString(1, name);
+                        ps.setString(2, password);
+                        i = ps.executeUpdate();
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            } finally {
+                try {
+                    if (con != null) {
+                        con.close();
+                    }
+                    if (ps != null) {
+                        ps.close();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if (i > 0) {
+            return "success";
+        } else
+            return "unsuccess";
+    }
+
+    public void dbData(String uName) {
+        if (uName != null) {
+            PreparedStatement ps;
+            Connection con;
+            ResultSet rs;
+
+            if (ds != null) {
+                try {
+                    con = ds.getConnection();
+                    if (con != null) {
+                        String sql = "select name,password from user where name = '"
+                                + uName + "'";
+                        ps = con.prepareStatement(sql);
+                        rs = ps.executeQuery();
+                        rs.next();
+                        dbName = rs.getString("name");
+                        dbPassword = rs.getString("password");
+                    }
+                } catch (SQLException sqle) {
+                    System.out.println("Kann mich nicht verbinden");
+                    sqle.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public String login() {
+        dbData(name);
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
+                .getExternalContext().getSession(false);
+        if (isLoginPage && (name.equals(dbName) && password.equals(dbPassword))) {
+            FacesContext.getCurrentInstance().getExternalContext()
+                    .getSessionMap().put("username", name);
+            if (session == null) {
+                FacesContext
+                        .getCurrentInstance()
+                        .getApplication()
+                        .getNavigationHandler()
+                        .handleNavigation(FacesContext.getCurrentInstance(),
+                                null, "/login.xhtml");
+            } else {
+                Object currentUser = session.getAttribute("name");
+                if (!isLoginPage && (currentUser == null || currentUser == "")) {
+                    FacesContext
+                            .getCurrentInstance()
+                            .getApplication()
+                            .getNavigationHandler()
+                            .handleNavigation(
+                                    FacesContext.getCurrentInstance(), null,
+                                    "/login.xhtml");
+                }
+            }
+            return "output";
+        } else {
+            return "invalid";
+        }
+    }
+
+    public void logout() {
+        FacesContext.getCurrentInstance().getExternalContext()
+                .invalidateSession();
+        FacesContext
+                .getCurrentInstance()
+                .getApplication()
+                .getNavigationHandler()
+                .handleNavigation(FacesContext.getCurrentInstance(), null,
+                        "/login.xhtml");
+    }
+}
